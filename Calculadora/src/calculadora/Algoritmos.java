a25385980d3bf55cbf19096696fa063fccc58bb9
Daniela020702 <98550464+Daniela020702@users.noreply.github.com>
2022-02-28 16:47:02 -0600

    Update Algoritmos.java

--- Calculadora/src/calculadora/Algoritmos.java
+++ Calculadora/src/calculadora/Algoritmos.java
@@ -94,4 +94,62 @@ public class Algoritmos {
         
         return resp;   
     }
+    public static boolean revisaPunto ( String revisa){
+        boolean resp=true;
+        int i=0,j=0,contador=0;
+       
+        while(j<revisa.length()&& resp){
+            while(i<revisa.length()&& contador<=1){
+                if(revisa.charAt(i)!='+'||revisa.charAt(i)!='-'||revisa.charAt(i)!='*'||revisa.charAt(i)!='^'||revisa.charAt(i)!='/')
+                    if(revisa.charAt(i)=='.')
+                        contador++;
+             i++;
+                }
+            if(contador>1)
+             resp=false;
+            j=i;
+            contador=0;
+        }
+         
+      
+        return resp;
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Prueba de analizaParentesis");
+        System.out.println("Con los paréntesis puestos correctamente");
+        System.out.println("1.1(a+b)  " + analizaParentesis("1(a+b)")); 
+        System.out.println("2. 6+(-1(-5(34-5)+3)) " +analizaParentesis("6+(-1(-5(34-5)+3.0))"));
+        
+        System.out.println("Con los paréntesis puestos incorrectamente");
+        System.out.println("1. 3)4-5+3( " + analizaParentesis("3)4-5+3("));
+        System.out.println("2. 34-5+3( " +analizaParentesis("34-5+3("));
+        System.out.println("3. 3)4-5+3 " +analizaParentesis("3)4-5+3--"));
+        System.out.println("4. 34-5+3( " +analizaParentesis("34-5+3("));
+        
+        
+        System.out.println("\nPrueba de analiza signo");
+        System.out.println("Con los signos puestos correctamente");
+        System.out.println("1. 1+7-58/6 " + revisaSignos("1+7-58/6")); 
+        System.out.println("2.34-5+3 " + revisaSignos("34-5+3.0"));
+        
+        System.out.println("Con los signos puestos incorrectamente");
+        System.out.println("1. 4/-5+3( " + revisaSignos("34/-5+3("));
+        System.out.println("2. 34-5+-3( " +revisaSignos("34-5+-3("));
+        System.out.println("3. 3**4-5+3 " +revisaSignos("3**4-5+3"));
+        
+        System.out.println("Prueba de revisaPunto");
+        System.out.println("Con los puntos puestos correctamente");
+        System.out.println("1.1(a+b)  " + revisaPunto("1(a+b)")); 
+        System.out.println("2. 6+(-1(-5.6(3.4-5)+3.0)) " +revisaPunto("6+(-1(-5(34-5)+3.0))"));
+        
+        System.out.println("Con los puntos puestos incorrectamente");
+        System.out.println("1. 3.2.4-5+3" + revisaPunto("3.2.4-5+3"));
+        System.out.println("2. 3.4.6-5+3 " +revisaPunto("3.4.6-5+3"));
+        System.out.println("3. .3.4-.5+3 " +revisaPunto(".3.4-.5+3"));
+        
+        
+    }
 }
